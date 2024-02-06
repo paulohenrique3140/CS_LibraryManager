a1b703082e4bce4bb79087aca4e51279323f2edd
Paulo Henrique <paulohenrique3140@outlook.com>
2024-02-06 00:27:56 -0300

    Testing commit

--- CS_LibraryManager/Program.cs
+++ CS_LibraryManager/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CS_LibraryManager {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello friend!");
+        }
+    }
+}
